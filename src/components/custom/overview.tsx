a30aeb60cb68afb799ba86fd0b5d2a6c8edc0ddc
이창현 <changhyeon743@gmail.com>
2025-04-27 11:57:20 +0900

    overview text

--- src/components/custom/overview.tsx
+++ src/components/custom/overview.tsx
@@ -14,17 +14,20 @@ export const Overview = () => {
             transition={{delay: 0.5}}
         >
             <div
-                className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-3xl text-muted-foreground">
-                <p className="text-xl font-semibold text-foreground flex flex-col gap-0.5">
+                className="rounded-xl p-6 flex flex-col gap-2 leading-relaxed text-center max-w-3xl text-gray-900">
+                <p className="text-xl font-bold text-foreground flex flex-col gap-0">
                     <CubeIcon className='size-8 mx-auto'/>
-                    나의 공간
-                    <span className='text-lg'>My spatial-</span>
+                    <span className='text-[15.5px]'>myspatial</span>
                 </p>
 
                 <p>
-                    나를 설명하는 것은 나, 그리고 나의 공간입니다.<br/>
+                    나를 설명하는 것은 나, 그리고 나의 공간입니다.<br/><br/>
                     이 시스템에는 '나'의 공간을 잘 찾을 수 있도록 대화형 에이전트가 잘 설계되어있습니다.<br/>
                     아래 템플릿을 활용하여 시작해보고, 편집해보세요.
+                </p>
+
+                <p className='text-xs text-muted-foreground'>
+                    ※이 프로젝트는 서울시립대학 공간정보공학과 졸업연구의 구현체입니다.
                 </p>
 
             </div>
